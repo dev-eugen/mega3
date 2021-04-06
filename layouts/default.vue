b787d7120734c966247af892675392ed376da319
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-04-06 18:53:22 +0300

    footer some edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
-    <Nuxt />
     <hero></hero>
-   
-    <footer> <Footer /></footer>
-
-   
-  </div> 
+    <Nuxt />
+    <Footer/>
+  </div>
 </template>
 <script>
-
 export default {
   components: {
     Hero: () => import("@/components/Sections/Hero"),
-    Footer: () =>import("@/components/Sections/Footer")
-   
-  }
-}
+    Footer: () => import("@/components/Sections/Footer"),
+  },
+};
 </script>
 
-
 <style lang="postcss">
-html{
-  @apply font-sans font-bold
+html {
+  @apply font-sans font-bold;
 }
 </style>

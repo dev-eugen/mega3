54738ff29d82ee0871bb4e10f593a302c4f6484c
Bohdan <bogdan@Bogdans-MacBook-Pro.local>
2021-04-13 09:54:53 +0300

    CategoryCard

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-        <div class="flex flec-col w-40">
-            <div class="w-max">
-                
-            </div>
-            <div class="w-max">
-                
-            </div>
-        </div>
+    <div class="text-center">
+        <a href="#" class="w-60 block mx-2 hover:opacity-80 duration-200">
+          <img src="img/catalog_img.png" alt="" class="h-80 w-full rounded-lg object-cover object-center">
+          <h6 class="text-green-dark text-xl font-bold text-center uppercase mt-3">jeans and snakers</h6>
+        </a>
     </div>
 </template>
 
